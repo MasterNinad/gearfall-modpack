946c81983e004e199fc9c322ebae387f758744f5
Ninad <kninad2008@gmail.com>
2026-03-30 22:37:44 +0530

    Added chocolates (create: confectionery) questline

--- kubejs/client_scripts/rename_items.js
+++ kubejs/client_scripts/rename_items.js
@@ -10,4 +10,7 @@ ClientEvents.lang('en_us', event => {
     event.renameItem('tfmg:etched_circuit_board', 'Etched Plastic Circuit Board');
     event.renameItem('tfmg:unfinished_circuit_board', 'Unfinished Basic Circuit Board');
     event.renameItem('tfmg:circuit_board', 'Basic Circuit Board');
+
+    //Create Confectionery: Items
+    event.renameItem('create_confectionery:black_chocolate_bucket', 'Dark Chocolate Bucket');
 })
